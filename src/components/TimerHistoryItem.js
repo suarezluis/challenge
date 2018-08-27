ed440af552a963ffdeea98f4275f6219c2189cc8
Luis Suarez <suarezluis@gmail.com>
2018-08-27 15:39:49 -0500

    Changed DESCRIPTION PLACEHOLDER for 'UNNAMED ENTRY' in order to make it more descriptive to the user

--- src/components/TimerHistoryItem.js
+++ src/components/TimerHistoryItem.js
@@ -36,7 +36,7 @@ export default class TimerHistoryItem extends Component {
 
     return (
       <div className="black-50 f6 flex justify-between w100 center bg-white br0 pa3 pa3-ns mv3 ba b--black-10">
-        <div>{description || "DESCRIPTION PLACEHOLDER"}</div>
+        <div>{description || "UNNAMED ENTRY"}</div>
         <div>{project && project.name}</div>
         <div>{renderCategories}</div>
         <div>{billable && "$"}</div>
